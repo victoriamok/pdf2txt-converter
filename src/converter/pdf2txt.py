2231134814f6fa4739fa72074489df520b6d8cc1
victoriamok <aprilvika@gmail.com>
2020-11-28 11:27:28 +0100

    update path

--- src/converter/pdf2txt.py
+++ src/converter/pdf2txt.py
@@ -39,8 +39,8 @@ def convert_multiple(pdf_dir, txt_dir):
 
 
 def main(argv):
-    pdf_dir = '/Users/viktoriiamokriak/Documents/Personal/CV/cv/'
-    txt_dir = '/Users/viktoriiamokriak/Documents/Personal/CV/cv/'
+    pdf_dir = '/your/path/to/pdf/files/'
+    txt_dir = '/your/path/to/txt/files/'
     try:
         opts, args = getopt.getopt(argv, 'ip:t:')
     except getopt.GetoptError:
